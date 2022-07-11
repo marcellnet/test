5a3cd8a5fb743c834519aa137db472aae9f3ca82
marcellnet <hanandiva30@gmail.com>
2022-07-11 17:57:02 +0700

    fix contact

--- main.js
+++ main.js
@@ -2,26 +2,18 @@ function buttonClick() {
   var nama = document.getElementById("nama");
   var nomer = document.getElementById("nomer");
   var pesan = document.getElementById("pesan");
-  var pesan = document.getElementById("pesan");
-  var form = document.forms["mrc-contact"];
-  var myAlert = document.getElementById("myalert");
-  var myAlertw = document.getElementById("myalertw");
-  // var sukses = document.getElementById("sukses");
-  // var errorNodes = document.getElementById("error");
 
   var gabungan = "Nama%20%3A%20" + nama.value + "%0ANomor%20%3A%20" + nomer.value + "%0APesan%20%3A%20" + pesan.value;
 
   var token = "5493881306:AAHK-1aPwfqTi4I0nLZc838tBq9WSS73yIA";
   var grup = "5111025809";
 
+  console.log("aoujsfhouihwqfuoiha");
+
   $.ajax({
     url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
     method: `POST`,
   });
-
-  form.reset();
-  myAlert.classList.toggle("d-none");
-  console.log("sukses test");
 }
 
 // on top
